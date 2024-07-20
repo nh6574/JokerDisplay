fab1ce23a8ac9a84afd0d8432660d18b27c6711a
Sebastián Venturino <venturinosebastian@gmail.com>
2024-07-20 12:38:12 -0300

    Capitalize Display :)

--- JokerDisplay.lua
+++ JokerDisplay.lua
@@ -2,7 +2,7 @@
 --- MOD_NAME: JokerDisplay
 --- MOD_ID: JokerDisplay
 --- MOD_AUTHOR: [nh6574]
---- MOD_DESCRIPTION: Display useful information under Jokers. Right-click on a Joker/Display to hide/show. Left-click on a display to collapse/expand.
+--- MOD_DESCRIPTION: Display useful information under Jokers. Right-click on a Joker/Display to hide/show. Left-click on a Display to collapse/expand.
 --- PRIORITY: -100000
 --- VERSION: 1.5.2
 
