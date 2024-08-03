9630600b30af0e75f8188d6c1339f4430cc8dea8
Sebastián Venturino <venturinosebastian@gmail.com>
2024-08-02 21:09:41 -0300

    just in case

--- JokerDisplay.lua
+++ JokerDisplay.lua
@@ -769,7 +769,7 @@ JokerDisplay.calculate_card_triggers = function(card, scoring_hand, held_in_hand
 
             if retrigger_function then
                 triggers = triggers +
-                    retrigger_function(card, scoring_hand, held_in_hand,
+                    retrigger_function(card, scoring_hand, held_in_hand or false,
                         v.joker_display_values and v.joker_display_values.blueprint_ability_joker or v)
             end
         end
