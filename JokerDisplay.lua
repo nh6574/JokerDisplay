7991733938e6b131302728e13845911bceafe660
Sebastián Venturino <venturinosebastian@gmail.com>
2024-08-11 15:24:15 -0300

    version_change

--- JokerDisplay.lua
+++ JokerDisplay.lua
@@ -6,7 +6,7 @@
 --- MOD_DESCRIPTION: Display useful information under Jokers. Right-click on a Joker/Display to hide/show. Left-click on a Display to collapse/expand.
 --- PRIORITY: -100000
 --- DEPENDENCIES: [Steamodded>=1.0.0-ALPHA-0805d]
---- VERSION: 1.6.4
+--- VERSION: 1.7.0
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
