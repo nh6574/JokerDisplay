997d474e68ce2c1e399d3523b46ba3132d3e425c
Mikhail Burykin <86657075+WholeHorse@users.noreply.github.com>
2025-01-07 20:00:55 +0300

    added ru localization

---
+++ localization/ru.lua
@@ -0,0 +1,23 @@
+return {
+	["misc"] = {
+		["dictionary"] = {
+			["jdis_enabled"] = "Включено",
+			["jdis_hide_by_default"] = "Скрывать по умолчанию",
+			["jdis_hide_empty"] = "Скрывать пустые",
+			["jdis_disable_collapse"] = "Отключить сворачивание",
+			["jdis_disable_perishable"] = "Отключить Временные",
+			["jdis_disable_rental"] = "Отключить Арендуемые",
+			["jdis_modifiers"] = "Модификаторы",
+			["jdis_reminders"] = "Напоминания",
+			["jdis_extras"] = "Экстра",
+			["jdis_default_display"] = "По умолчанию",
+			["jdis_small_display"] = "Свернутый вид",
+			["jdis_active"] = "Активно!",
+			["jdis_inactive"] = "Неактивно",
+			["jdis_all_suits"] = "Все масти"
+		},
+		["v_dictionary"] = {
+			["jdis_odds"] = "#1# к #2#"
+		}
+	},
+}
