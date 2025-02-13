6b375f25aedc1ce3b7f269aab85086bfe9b04b14
Sebastián Venturino <venturinosebastian@gmail.com>
2025-02-13 16:45:30 -0300

    clean up

--- localization/default.lua
+++ localization/default.lua
@@ -1,7 +1,7 @@
 return {
-	["descriptions"] = {
-		["Mod"] = {
-			["JokerDisplay"] = {
+	descriptions = {
+		Mod = {
+			JokerDisplay = {
                 name = "JokerDisplay",
                 text = {
                     "Displays useful information under Jokers",
@@ -18,25 +18,25 @@ return {
             }
 		}
 	},
-	["misc"] = {
-		["dictionary"] = {
-			["jdis_enabled"] = "Enabled",
-			["jdis_hide_by_default"] = "Hide by default",
-			["jdis_hide_empty"] = "Hide empty",
-			["jdis_disable_collapse"] = "Disable collapsing",
-			["jdis_disable_perishable"] = "Disable Perishable",
-			["jdis_disable_rental"] = "Disable Rental",
-			["jdis_modifiers"] = "Modifiers",
-			["jdis_reminders"] = "Reminders",
-			["jdis_extras"] = "Extras",
-			["jdis_default_display"] = "Default",
-			["jdis_small_display"] = "Collapsed",
-			["jdis_active"] = "Active!",
-			["jdis_inactive"] = "Inactive",
-			["jdis_all_suits"] = "All Suits"
+	misc = {
+		dictionary = {
+			jdis_enabled = "Enabled",
+			jdis_hide_by_default = "Hide by default",
+			jdis_hide_empty = "Hide empty",
+			jdis_disable_collapse = "Disable collapsing",
+			jdis_disable_perishable = "Disable Perishable",
+			jdis_disable_rental = "Disable Rental",
+			jdis_modifiers = "Modifiers",
+			jdis_reminders = "Reminders",
+			jdis_extras = "Extras",
+			jdis_default_display = "Default",
+			jdis_small_display = "Collapsed",
+			jdis_active = "Active!",
+			jdis_inactive = "Inactive",
+			jdis_all_suits = "All Suits"
 		},
-		["v_dictionary"] = {
-			["jdis_odds"] = "#1# in #2#"
+		v_dictionary = {
+			jdis_odds = "#1# in #2#"
 		}
 	},
 }
