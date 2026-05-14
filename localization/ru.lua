abf9116e9677a2789aaa6a9bdfe813213e4e80ca
amnesiaof <dolombovskyyalexs@gmail.com>
2026-05-14 18:41:48 +0300

    Add Russian localization entries

--- localization/ru.lua
+++ localization/ru.lua
@@ -1,9 +1,37 @@
 return {
+	descriptions = {
+		Mod = {
+			JokerDisplay = {
+				name = "JokerDisplay",
+				text = {
+					"Показывает полезную информацию под джокерами",
+					" ",
+					"{C:attention}Управление:{}",
+					"{C:blue}ПКМ или B:{} скрыть/показать",
+					"{C:blue}ЛКМ или вверх:{} свернуть/развернуть",
+					" ",
+					"Особая благодарность {C:green}Eremel{} и {C:green}OppositeWolf770{}",
+					" ",
+					"Русская локализация: {C:green}WholeHorse{}",
+					"Французская локализация: {C:green}SDM_0{}",
+					"Португальская локализация: {C:green}naumazeredo{}",
+					"Итальянская локализация: {C:green}fleshness{}",
+					"Немецкая локализация: {C:green}GunnableScum{}",
+					"Китайская (трад.) локализация: {C:green}wilfredlam0418{}",
+					"Нидерландская локализация: {C:green}Soulsphere{}",
+					"Китайская (упр.) локализация: {C:green}ChromaPIE",
+					"Японская локализация: {C:green}koukichi_kkc",
+				},
+			}
+		}
+	},
 	misc = {
 		dictionary = {
 			jdis_enabled = "Включено",
 			jdis_hide_by_default = "Скрывать по умолчанию",
 			jdis_hide_empty = "Скрывать пустые",
+			jdis_shift_to_hide = "Shift + ПКМ для скрытия",
+			jdis_joker_count = "Показывать слоты джокеров",
 			jdis_disable_collapse = "Отключить сворачивание",
 			jdis_disable_perishable = "Отключить Временные",
 			jdis_disable_rental = "Отключить Арендуемые",
@@ -18,7 +46,8 @@ return {
 			jdis_author = 'Автор',
 		},
 		v_dictionary = {
-			jdis_odds = "#1# к #2#"
+			jdis_odds = "#1# к #2#",
+			jdis_rank_of_suit = "#1# #2#"
 		}
 	},
 }
