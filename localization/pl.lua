3e922a17c9410ec9d65a9f64e0b7dd1dbaae937d
MaxKolac <maksymilian.kolacinski@gmail.com>
2026-06-19 23:53:15 +0200

    Add polish localization

---
+++ localization/pl.lua
@@ -0,0 +1,54 @@
+return {
+	descriptions = {
+		Mod = {
+			JokerDisplay = {
+				name = "JokerDisplay",
+				text = {
+					"Wyświetla użyteczne informacje pod Jokerami",
+					" ",
+					"{C:attention}Sterowanie:{}",
+					"{C:blue}Prawy przycisk myszy lub B:{} pokaż/ukryj",
+					"{C:blue}Lewy przycisk myszy lub Góra:{} zwiń/rozwiń",
+					" ",
+					"Szczególne podziękowania dla {C:green}Eremel{} oraz {C:green}OppositeWolf770{}",
+					" ",
+					"Przekład na rosyjski: {C:green}WholeHorse{}",
+					"Przekład na francuski: {C:green}SDM_0{}",
+					"Przekład na portugalski: {C:green}naumazeredo{}",
+					"Przekład na włoski: {C:green}fleshness{}",
+					"Przekład na niemiecki: {C:green}GunnableScum{}",
+					"Przekład na chiński tradycyjny: {C:green}wilfredlam0418{}",
+					"Przekład na holenderski: {C:green}Soulsphere{}",
+					"Przekład na chiński uproszczony: {C:green}ChromaPIE",
+					"Przekład na japoński: {C:green}koukichi_kkc",
+					"Przekład na polski: {C:green}Sagiritarius{}"
+				},
+			}
+		}
+	},
+	misc = {
+		dictionary = {
+			jdis_enabled = "Włączony",
+			jdis_hide_by_default = "Ukrywaj domyślnie",
+			jdis_hide_empty = "Ukrywaj puste",
+			jdis_shift_to_hide = "Shift + Prawy przycisk myszy aby ukryć",
+			jdis_joker_count = "Pokaż ilość slotów na Jokery nad informacjami",
+			jdis_disable_collapse = "Wyłącz zwijanie",
+			jdis_disable_perishable = "Wyłącz dla nietrwałych Jokerów",
+			jdis_disable_rental = "Wyłącz dla wypożyczonych Jokerów",
+			jdis_modifiers = "Modyfikatory",
+			jdis_reminders = "Przypomnienia",
+			jdis_extras = "Dodatkowe informacje",
+			jdis_default_display = "Domyślny",
+			jdis_small_display = "Zwinięty",
+			jdis_active = "Aktywny!",
+			jdis_inactive = "Nieaktywny",
+			jdis_all_suits = "Wszystkie kolory",
+			jdis_author = "Autor"
+		},
+		v_dictionary = {
+			jdis_odds = "#1# na #2#",
+			jdis_rank_of_suit = "#1# z #2#"
+		}
+	},
+}
