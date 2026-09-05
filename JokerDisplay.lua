ea799be32c0a0345a54cfd44e468bdf0f7fe08b1
N <venturinosebastian@gmail.com>
2026-09-05 12:14:10 -0300

    test please ignore

--- JokerDisplay.lua
+++ JokerDisplay.lua
@@ -13,3 +13,5 @@ JokerDisplay.Global_Definitions = SMODS.load_file("definitions/global_definition
 JokerDisplay.Definitions = SMODS.load_file("definitions/display_definitions.lua")() or {}
 JokerDisplay.Blind_Definitions = SMODS.load_file("definitions/blind_definitions.lua")() or {}
 JokerDisplay.Edition_Definitions = SMODS.load_file("definitions/edition_definitions.lua")() or {}
+
+assert(nil)
